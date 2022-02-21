8f10671b056e291e46059f8507f244147ad76c55
anasenguady <anas.en-guady@etud.univ-jfc.fr>
2022-02-21 14:09:06 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="header">
+      <div> logo monde </div>
+      
+      <div> argent </div>
+      <div> multiplicateur </div>
+      <div> ID du joueur </div>
+
+
+
+    </div>
+  );
+}
+
+export default App;
