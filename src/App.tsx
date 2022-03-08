757a15afeb29bd1f4bdca1ec2bc19d00b359de62
anasenguady <anas.en-guady@etud.univ-jfc.fr>
2022-03-08 14:10:33 +0100

    lucas

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Theheader from "./components/theheader"
+import myCryptos from "./components/cryptos"
+import MyCryptos from './components/cryptos';
+
 
 function App() {
   return (
-    <div className="header">
-      <div> logo monde </div>
-      
-      <div> argent </div>
-      <div> multiplicateur </div>
-      <div> ID du joueur </div>
-
+    <div>
+      <Theheader/>
+      <MyCryptos/>
 
 
     </div>
+
+
+
+
+    
+
+    
   );
 }
+
 
 export default App;
